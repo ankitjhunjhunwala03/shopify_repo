1c46c5161dd4c92e56e31d9c6a4b020a0ea3f859
Tea Box <ankit.jhunjhunwala@teabox.com>
2017-11-22 19:45:45 +0530

    add email template

--- apps/tmail/views.py
+++ apps/tmail/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.core.mail import send_mail
 from django.http.response import JsonResponse
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 # Create your views here.
 
@@ -17,14 +19,26 @@ def get_value_from_dict(key, dictionary):
 def trigger_email(request):
     data = request.POST
     try:
+        bcc = None
+        context = {}
         to_email = get_value_from_dict('to_email', data)
+        to_email = to_email.split(',')
         from_email = get_value_from_dict('from_email', data)
         subject = get_value_from_dict('subject', data)
         body = get_value_from_dict('body', data)
     except Exception, e:
         return JsonResponse({'status': '0', 'error': str(e)})
     try:
-        send_mail(subject, body, from_email, [to_email], fail_silently=False)
+        msg = EmailMultiAlternatives(
+            subject, body, from_email, to_email, bcc)
+        msg.encoding = "utf-8"
+        msg.content_subtype = "html"
+        try:
+            html_content = render_to_string(body, context)
+            msg.attach_alternative(html_content, "text/html")
+        except Exception, e:
+            print str(e)
+        msg.send()
     except Exception, e:
         return JsonResponse({'status': '0', 'error': str(e)})
     return JsonResponse({'status': '1'})
